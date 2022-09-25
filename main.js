c57b46085bb2e6b41a36d73bb8914dd862bf2f7d
sikandraomprasad <89696118+sikandraomprasad@users.noreply.github.com>
2022-09-25 07:09:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+mustache_x=0
+mustache_y=0
+
+function preload()
+{
+  mustache=loadImage("https://i.postimg.cc/QtDYRxrf/m.png")
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+    image(video,0,0,300,300);
+    image(mustache,mustache_x,mustache_y,60,60);
+}
+
+function take_snapshot()
+{
+    save("my_picture.png");
+}
+
+function modelLoaded()
+{
+  console.log('poseNet Is Initialized')
+}
+
+function gotPoses(results)
+{
+if(results.length > 0)
+  console.log("result");
+  mustache_x=results[0].pose.nose.x-30;
+  mustache_y=results[0].pose.nose.y;
+  console.log("nose x =" +results[0].pose.nose.x);
+  console.log("nose y =" +results[0].pose.nose.y);
+}
